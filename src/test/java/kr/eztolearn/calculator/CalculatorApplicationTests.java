ca3cbc6c9571c26656f1c661a6f4b9961cbb49c5
정인상 <golsung@naver.com>
2023-03-02 14:02:03 +0900

    added acceptance test

--- src/test/java/kr/eztolearn/calculator/CalculatorApplicationTests.java
+++ src/test/java/kr/eztolearn/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,33 @@
 package kr.eztolearn.calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		classes = CalculatorApplication.class)
 class CalculatorApplicationTests {
 
+	@Autowired
+	TestRestTemplate testRestTemplate;
+
+	private final String  server = System.getProperty("calculator.url");
+
+	@LocalServerPort
+	int randomServerPort;
+
 	@Test
-	void contextLoads() {
+	void goHome() {
+		System.out.println(server);
+		String result = testRestTemplate.getForObject(server+":"+randomServerPort, String.class);
+		assertEquals("Welcome GitHub Action Home", result);
 	}
 
 }
