25bbe6088307a585f850f25d766c5a081aaf5315
정인상 <golsung@naver.com>
2023-03-02 11:21:29 +0900

    added rest controller

--- src/main/java/kr/eztolearn/calculator/Calculator.java
+++ src/main/java/kr/eztolearn/calculator/Calculator.java
@@ -2,20 +2,21 @@ package kr.eztolearn.calculator;
 
 public class Calculator {
 
-        private int a;
-        private int b;
-        private int sum;
+    private int a;
+    private int b;
+    private int sum;
 
-        public Calculator() {}
+    public Calculator() {
+    }
 
-        public Calculator(int a, int b) {
-            this.a = a;
-            this.b = b;
-        }
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
 
-        public int getSum() {
-            this.sum = a + b;
-            return this.sum;
-        }
+    public int getSum() {
+        this.sum = a + b;
+        return this.sum;
+    }
 
 }
---
+++ src/main/java/kr/eztolearn/calculator/CalculatorController.java
@@ -0,0 +1,22 @@
+package kr.eztolearn.calculator;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class CalculatorController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Welcome GitHub Action Home";
+    }
+
+    @RequestMapping("/sum")
+    public int sum(@RequestParam("a") int a, @RequestParam("b") int b) {
+        Calculator calculator = new Calculator(a, b);
+
+        return calculator.getSum();
+    }
+
+}
